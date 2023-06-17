2ed46b23bc99561a34cb0e44562060614dc5cfba
alexander231 <ggal2426@gmail.com>
2023-06-17 13:38:51 +0300

    create consumer producer services

---
+++ cmd/consumer/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/alexander231/rabbitmq/internal"
+	"github.com/joho/godotenv"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASS")
+
+	conn, err := internal.ConnectRabbitMQ(username, password, "localhost:5672", "customers")
+	if err != nil {
+		panic(err)
+	}
+
+	mqClient, err := internal.NewRabbitMQClient(conn)
+	if err != nil {
+		panic(err)
+	}
+
+	messageBus, err := mqClient.Consume("customers_created", "email-service", false)
+	if err != nil {
+		panic(err)
+	}
+
+	// blocking is used to block forever
+	var blocking chan struct{}
+
+	// go func() {
+	// 	for message := range messageBus {
+	// 		// breakpoint here
+	// 		log.Printf("New Message: %v", string(message.Body))
+
+	// 		// Multiple means that we acknowledge a batch of messages, leave false for now
+	// 		if err := message.Ack(false); err != nil {
+	// 			log.Printf("Acknowledged message failed: Retry ? Handle manually %s\n", message.MessageId)
+	// 			continue
+	// 		}
+	// 		log.Printf("Acknowledged message %s\n", message.MessageId)
+	// 	}
+	// }()
+
+	// Set a timeout for 15 secs
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+	// Create an Errgroup to manage concurrecy
+	g, ctx := errgroup.WithContext(ctx)
+	// Set amount of concurrent tasks
+	g.SetLimit(10)
+	go func() {
+		for message := range messageBus {
+			// Spawn a worker
+			msg := message
+			g.Go(func() error {
+				log.Printf("New Message: %v", msg)
+
+				time.Sleep(10 * time.Second)
+				// Multiple means that we acknowledge a batch of messages, leave false for now
+				if err := msg.Ack(false); err != nil {
+					log.Printf("Acknowledged message failed: Retry ? Handle manually %s\n", msg.MessageId)
+					return err
+				}
+				log.Printf("Acknowledged message %s\n", msg.MessageId)
+				return nil
+			})
+		}
+	}()
+
+	log.Println("Consuming, to close the program press CTRL+C")
+	// This will block forever
+	<-blocking
+
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module github.com/alexander231/rabbitmq
 go 1.19
 
 require (
+	github.com/rabbitmq/amqp091-go v1.8.1
+	github.com/rs/zerolog v1.29.1
+)
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/rs/zerolog v1.29.1 // indirect
+	golang.org/x/sync v0.3.0
 	golang.org/x/sys v0.9.0 // indirect
 )
