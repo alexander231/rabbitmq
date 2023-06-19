a5ab9064e9d5ec90dffa79187f4c06c4714c36e4
alexander231 <ggal2426@gmail.com>
2023-06-19 14:41:35 +0300

    update producer to publish; consume message callbacks

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"time"
 
@@ -31,9 +32,33 @@ func main() {
 	failOnError(err, "failed to create new rabittmq connection")
 	defer conn.Close()
 
+	consumeConn, err := internal.ConnectRabbitMQ(username, password, "localhost:5672", "customers")
+	failOnError(err, "failed to create new rabittmq consume connection")
+	defer consumeConn.Close()
+
 	client, err := internal.NewRabbitMQClient(conn)
 	failOnError(err, "failed to create new rabbitmq client")
 	defer client.Close()
+
+	consumeClient, err := internal.NewRabbitMQClient(conn)
+	failOnError(err, "failed to create new rabbitmq consumeClient")
+	defer consumeClient.Close()
+
+	// Create Unnamed Queue which will generate a random name, set AutoDelete to True
+	queue, err := consumeClient.CreateQueue("", true, true)
+	failOnError(err, "failed to create queue")
+
+	err = consumeClient.CreateBinding(queue.Name, queue.Name, "customer_callbacks")
+	failOnError(err, "failed to create binding between queue and customer_callbacks exchange")
+
+	messageBus, err := consumeClient.Consume(queue.Name, "customer-api", true)
+	failOnError(err, "failed to create message bus to consume messages from the publisher side")
+
+	go func() {
+		for message := range messageBus {
+			log.Printf("Message Callback %s\n", message.CorrelationId)
+		}
+	}()
 
 	// Create context to manage timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -45,9 +70,17 @@ func main() {
 			ContentType:  "text/plain",       // The payload we send is plaintext, could be JSON or others..
 			DeliveryMode: amqp091.Persistent, // This tells rabbitMQ that this message should be Saved if no resources accepts it before a restart (durable)
 			Body:         []byte("An cool message between services"),
+			// We add a REPLYTO which defines the
+			ReplyTo: queue.Name,
+			// CorrelationId can be used to know which Event this relates to
+			CorrelationId: fmt.Sprintf("customer_created_%d", i),
 		})
 		failOnError(err, "failedd to send message to customers_events exchange")
 	}
 
-	log.Info().Msgf("%v", client)
+	var blocking chan struct{}
+
+	log.Info().Msg("Waiting on Callbacks, to close the program press CTRL+C")
+	// This will block forever
+	<-blocking
 }
